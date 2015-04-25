dfee7757b534933790e8001e7f5f0e0cb2132d3d
Fabio Arcidiacono <fa.arcidiacono@gmail.com>
2015-04-25 12:26:32 +0200

    fix generated file name

--- run-azure.sh
+++ run-azure.sh
@@ -3,7 +3,7 @@
 printf '\nExcuting benchmark over Azure Tables [low-level API]\n'
 # execute benchmark over azure tables low level API
 java -cp target/kundera-benchmarks-1.0-SNAPSHOT.jar:target/dependency/* com.yahoo.ycsb.Client -t -db it.polimi.ycsb.database.AzureTableClient -P src/main/resources/workloads/workloadinsert100K -P src/main/resources/azure.properties -s -threads 30 -load > ../load_lowlevelAPI_azure
-java -cp target/kundera-benchmarks-1.0-SNAPSHOT.jar:target/dependency/* com.yahoo.ycsb.Client -t -db it.polimi.ycsb.database.AzureTableClient -P src/main/resources/workloads/workloadinsert100K -P src/main/resources/azure.properties -s -threads 30 -t > ../load_lowlevelAPI_azure
+java -cp target/kundera-benchmarks-1.0-SNAPSHOT.jar:target/dependency/* com.yahoo.ycsb.Client -t -db it.polimi.ycsb.database.AzureTableClient -P src/main/resources/workloads/workloadinsert100K -P src/main/resources/azure.properties -s -threads 30 -t > ../transaction_lowlevelAPI_azure
 
 printf '\n\nExcuting benchmark over Azure Tables [Kundera]\n'
 # execute benchmark over azure tables kundera version
